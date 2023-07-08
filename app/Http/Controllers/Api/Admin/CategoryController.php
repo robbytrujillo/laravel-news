740cc8499a558e3c593bff1ee24569a9f5543841
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-07-08 11:46:58 +0700

    laravel news | Category Controller

--- app/Http/Controllers/Api/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -115,6 +115,38 @@ public function update(Request $request, Category $category) {
         ]);
         
     }
+
+    //update category without image
+    $category->update([
+        'name' => $request->name,
+        'slug' => Str::slug($request->name, '-'),
+    ]);
+
+    if($category) {
+        //return success with Api Resource
+        return new CategoryResource(true, 'Data Category Berhasil Diupdate!', $category);
+    }
+
+    //return failed with Api Resource
+    return new CategoryResource(false, 'Data Category Gagal Diupdate!', null);
 }
 
+/**
+ * Remove the specified resource from storage.
+ * 
+ * @param int $id
+ * @return \Illuminate\Http\Response
+ */
+public function destroy(Category $category){
+    //remove image
+    Storage::disk('local')->delete('public/categories/'.basename($category->image));
+
+    if($category->delete()) {
+        //return success with Api Resource
+        return new CategoryResource(true, 'Data Category Berhasil Dihapus!', null); 
+    }
+
+    //return failed with Api Resource
+    return new CategoryResource(false, 'Data Category Gagal Dihapus!',null );
+}
 }
